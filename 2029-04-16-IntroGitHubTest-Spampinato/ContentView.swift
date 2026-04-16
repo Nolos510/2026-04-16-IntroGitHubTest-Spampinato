2f4af494156d89fedbffa5fbd6e4024b935b1b39
Carlo Spampinato <Knolos510@gmail.com>
2026-04-16 16:29:34 -0700

    Initial Commit

---
+++ 2029-04-16-IntroGitHubTest-Spampinato/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  2029-04-16-IntroGitHubTest-Spampinato
+//
+//  Created by patron on 4/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2029-04-16-IntroGitHubTest-Spampinato/_029_04_16_IntroGitHubTest_SpampinatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  _029_04_16_IntroGitHubTest_SpampinatoApp.swift
+//  2029-04-16-IntroGitHubTest-Spampinato
+//
+//  Created by patron on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct _029_04_16_IntroGitHubTest_SpampinatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
